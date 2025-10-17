69b24afec01b99d3009c025e28c071b415f0c98d
Nizar Louhichi <37078717+NizarNi@users.noreply.github.com>
2025-10-17 10:32:17 +0200

    Improve rolling xG reuse and add lightweight stubs

---
+++ understat/__init__.py
@@ -0,0 +1,9 @@
+class Understat:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    async def get_teams(self, *args, **kwargs):
+        return []
+
+    async def get_league_results(self, *args, **kwargs):
+        return []
